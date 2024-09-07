fcee8674a1d1c41b640896d03070b476187d25fe
T_cat9000_2 <terpstragraham@gmail.com>
2024-09-07 10:43:59 -0700

    Update follow.py

--- follow.py
+++ follow.py
@@ -5,7 +5,7 @@ import time
 print("Creating session")
 session = attach.login("huh", "huh")
 print("Connecting studio")
-studio = session.connect_studio("34868267")
+studio = session.connect_studio("35292763")
 off = 14480
 index = 0
 while True:
@@ -16,7 +16,8 @@ while True:
         project = session.connect_project(project_info['id'])
         project.get_author().follow()
         print(f"{index}: Followed author {project.get_author().username} from project {project.title}")
-        user = session.connect_user({project.get_author().username})
+        user = session.connect_user(project.get_author().username)
         user.post_comment("my script says i followed you! i am Act3nium for more info on me go to my profile. (sometimes this script is wrong! this bot might not have followed you!)")
+        time.sleep(1)
     off += 20
     time.sleep(60)
